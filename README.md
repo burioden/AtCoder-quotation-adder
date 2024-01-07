87cc0b8774b789c03b544d31e58af2b45e6eb42c
burioden <hashizoe.y@gmail.com>
2024-01-08 04:35:10 +0900

    🎉新規追加りりーす

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # AtCoder-quotation-adder
+
+AtCoderの問題文中の文字列に""をつけてくれるしあわせスクリプトです。  
+A問題がもっと爆速になるね！
+
+[Tampermonkey](https://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=ja) などを先にインストールしてお使いください。
+
+[Tampermonkeyへのインストール](https://github.com/burioden/AtCoder-quotation-adder/script.user.js)
+
+
+## 更新履歴
+- 2024/01/08 : ver1.0 release
---
+++ script.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         AtCoder quotation adder
+// @namespace    https://github.com/burioden
+// @version      1.0
+// @description  AtCoderの問題文中の文字列に""をつけてくれます。要素の中身が数値のみのときは""をつけません。
+// @author       burioden
+// @match        https://atcoder.jp/contests/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=atcoder.jp
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // 問題内の<code>要素を取得
+    const codes = document.querySelectorAll('#task-statement .part code');
+
+    // 文字列かつ数値ではないか判定
+    function isStrAndNotNum(val) {
+        const flg = typeof val === 'string' && val.trim() !== '' && isNaN(val);
+        return flg;
+    }
+
+    // 文字列のみダブルクォーテーションで囲む
+    codes.forEach(function(elm) {
+        const text = elm.textContent;
+
+        if (isStrAndNotNum(text)) {
+            elm.textContent = '"' + text + '"';
+        }
+    });
+
+})();
